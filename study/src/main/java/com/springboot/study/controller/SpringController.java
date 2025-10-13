5bbc3165be13057a9323e211fc6a949bb0020ada
Ha <a84656607@gmail.com>
2025-10-13 16:21:32 +0900

    2025-10-13 commit

---
+++ study/src/main/java/com/springboot/study/controller/SpringController.java
@@ -0,0 +1,16 @@
+package com.springboot.study.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SpringController {
+    @GetMapping("/spring")
+    public String spring() {
+        System.out.println("----> SpringController!!!");
+        return "spring"; //view name
+    }
+}
+
+
+
